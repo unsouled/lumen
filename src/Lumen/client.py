d53eb7b0a51fcbd0f3196ded9cc7e1800ab3b99c
Freddie Park <sorelove@gmail.com>
2011-08-23 19:32:11 +0900

    renamed member variables.

--- src/Lumen/client.py
+++ src/Lumen/client.py
@@ -9,8 +9,8 @@ class Client():
     def __init__(self, clientId):
         self.id = clientId
         self.responses = []
-        self.received = []
-        self.httpRequest = None
+        self.receivedMessages = []
+        self.connectRequest = None
         self.channelsSubscribing = set()
 
     def handleMessage(self, msg):
@@ -18,25 +18,24 @@ class Client():
 
     def _doHandle(self, msg):
         responses = msg.handle(self)
-        hasConnect = False
         chs = [response['channel'] for response in responses]
         if '/meta/connect' not in chs:
             msg.httpRequest.write(JSONEncoder().encode(responses))
             msg.httpRequest.finish()
         else:
-            self.httpRequest = msg.httpRequest
+            self.connectRequest = msg.httpRequest
             self.responses = responses
             self.publish([])
 
     def publish(self, msg):
-        self.received.extend(msg)
-        if self.received and self.httpRequest:
-            self.responses.extend(self.received)
-            self.httpRequest.write(JSONEncoder().encode(self.responses))
-            self.httpRequest.finish()
+        self.receivedMessages.extend(msg)
+        if self.receivedMessages and self.connectRequest:
+            self.responses.extend(self.receivedMessages)
+            self.connectRequest.write(JSONEncoder().encode(self.responses))
+            self.connectRequest.finish()
             self.responses = []
-            self.received = []
-            self.httpRequest = None
+            self.receivedMessages = []
+            self.connectRequest = None
 
     def subscribe(self, ch):
         self.channelsSubscribing.add(ch)
