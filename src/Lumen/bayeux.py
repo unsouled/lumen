b957ae37257ffde596c30426098a25e67276280a
unsouled <unsouled0@gmail.com>
2011-09-09 16:59:47 +0900

    find resource file in package

--- src/Lumen/bayeux.py
+++ src/Lumen/bayeux.py
@@ -3,6 +3,7 @@ import twisted.web.server
 import webconsole
 import config
 from message import Message
+from pkg_resources import resource_filename
 
 class Bayeux(resource.Resource):
     class Server(resource.Resource):
@@ -21,7 +22,7 @@ class Bayeux(resource.Resource):
         resource.Resource.__init__ (self)
         self.putChild(config.getConfig().get('default', 'endpoint'), Bayeux.Server())
         self.putChild(config.getConfig().get('webconsole', 'endpoint'), webconsole.WebConsole())
-        self.putChild('public', static.File('res/public'))
+        self.putChild('public', static.File(resource_filename(__name__, 'res/public')))
 
     def render_GET(self, request):
         pass
